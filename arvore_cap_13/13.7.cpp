a6063cd9457fc6f98d773ac69c4c9f2c63d18da5
Diego de Sousa Araujo <diesgo.araujo@gmail.com>
2023-03-10 23:55:19 -0300

    Fixing the problems on the verification if two trees are equal

--- arvore_cap_13/13.7.cpp
+++ arvore_cap_13/13.7.cpp
@@ -1,14 +1,25 @@
 #include "arvore.hpp"
 
-int iguais(NoArv* raiz_a, NoArv* raiz_b) {
+bool iguais(NoArv* raiz_a, NoArv* raiz_b) {
+    if (raiz_a == NULL && raiz_b == NULL) {
+        return 1;
+    }
+
+    if (is_null(raiz_a) || is_null(raiz_b)) {
+        return 0;
+    }
     if (raiz_a->info == raiz_b->info) {
         if (iguais(raiz_a->esquerda, raiz_b->esquerda) && iguais(raiz_a->direita, raiz_b->direita)) {
             return 1;
-        }
-    }
+        };
+        return 0;
+    } 
 
     return 0;
-}
+
+} 
+
+
 
 int main() {
     NoArv* raiz_a = criarArvore(
@@ -19,9 +30,8 @@ int main() {
     
     NoArv* raiz_b = criarArvore(
         'a', criarArvore('b', NULL, NULL), criarArvore(
-        'c', criarArvore('d', NULL, NULL), criarArvore(
-        'e', NULL, NULL
-        )));
+        'c', criarArvore('d', NULL, NULL), NULL
+        ));
 
    /*  NoArv* raiz = criarArvore(
         'a', criarArvore('b', NULL, NULL), criarArvore(
